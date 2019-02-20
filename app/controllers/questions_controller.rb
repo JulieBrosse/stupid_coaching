3ad8924579e0586e18a7286c003ad42cf4c95028
Julie Brosse <juliebrosse31@hotmail.fr>
2019-02-20 14:48:35 +0100

    exercice ok - back

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+
+  def ask
+ #   @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great !"
+    elsif @question.last == "?"
+      @answer = "Silly Question"
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
